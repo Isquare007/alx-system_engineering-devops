73efac8e9d756b5d7ac4ae211ff0d0af1fbd0457
Isquare007 <akibrahiim2019@gmail.com>
2022-10-18 18:14:20 +0000

    YELL

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Outputs: [SENDER],[RECEIVER],[FLAGS]
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
