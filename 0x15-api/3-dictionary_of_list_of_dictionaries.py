5db2777c7f8bb8a3b965bb5a983e3cf86c5828b4
Isquare007 <akibrahiim2019@gmail.com>
2023-01-16 21:04:02 +0000

    all employees todo

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,14 +13,19 @@ if __name__ == '__main__':
         "{}/users".format(url)).json()
     for employ in employee:
         employeeId = employ.get('id')
-        todos = requests.get("{}/todos".format(url), params={"userId": employeeId},).json()
+        todos = requests.get(
+            "{}/todos".format(url),
+            params={
+                "userId": employeeId},
+        ).json()
 
         userName = employ.get('username')
 
-        json_list = [{'username': userName, 'task': data.get("title"), "completed": data.get("completed")} for data in todos]
+        json_list = [{'username': userName, 'task': data.get(
+            "title"), "completed": data.get("completed")} for data in todos]
 
         json_f[employeeId] = json_list
-    
+
     filename = "todo_all_employees" + ".json"
 
     with open(filename, 'w') as f:
