ee9c19dc8a6a7611cb1f402ad36c69c912d06cca
Isquare007 <akibrahiim2019@gmail.com>
2023-01-16 18:30:43 +0000

    1 export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,15 +19,20 @@ if __name__ == '__main__':
             done_task.append(data.get('title'))
 
     userId = employee.get('id')
-    
+
     csv_list = []
     for data in todos:
-        csv_list.append([data.get("userId"), employee.get("username"), data.get("completed"), data.get("title")])
+        csv_list.append([data.get("userId"), employee.get(
+            "username"), data.get("completed"), data.get("title")])
 
     filename = "{}.csv".format(userId)
 
     with open(filename, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        csvwriter = csv.writer(
+            csvfile,
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_ALL)
 
         for line in csv_list:
             csvwriter.writerow(line)
