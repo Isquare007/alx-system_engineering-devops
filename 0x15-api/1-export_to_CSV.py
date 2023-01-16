6e72640fded9f05c2dc5ca56a8312d2d09b46acf
Isquare007 <akibrahiim2019@gmail.com>
2023-01-16 19:16:53 +0000

    mh

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,30 +2,29 @@
 """
 a script that returns info about a customer's todo list
 """
+import csv
 import requests
 from sys import argv
-import csv
 
 if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com"
     employeeId = argv[1]
 
-    employee = requests.get("{}/users/{}".format(url, employeeId)).json()
-    todos = requests.get(url + "/todos", params={"userId": employeeId}).json()
+    employee = requests.get("{}/users/{}".format(url, employeeId), verify=False).json()
+    todos = requests.get("{}/todos".format(url), params={"userId": employeeId}, verify=False).json()
 
     done_task = []
     for data in todos:
         if data.get('completed') is True:
             done_task.append(data.get('title'))
 
-    userId = employee.get('id')
+    userName = employee.get('username')
 
     csv_list = []
     for data in todos:
-        csv_list.append([data.get("userId"), employee.get(
-            "username"), data.get("completed"), data.get("title")])
+        csv_list.append([employeeId, userName, data.get("completed"), data.get("title")])
 
-    filename = "{}.csv".format(userId)
+    filename = "{}.csv".format(employeeId)
 
     with open(filename, 'w', newline='') as csvfile:
         csvwriter = csv.writer(
