b47a4a686ebf435b97aba178ecd10b56301fb3b2
Isquare007 <akibrahiim2019@gmail.com>
2023-01-31 13:17:25 +0000

    reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,9 @@ import requests
 def number_of_subscribers(subreddit):
     """returns the number of subscribers (not active users,
     total subscribers)"""
-    link = argv[1]
-    url = "https://www.reddit.com/r/" + link + "/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = {"User-Agent": "xica369"}
+    headers = {"User-Agent": "My Reddit API Client/1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if (response.status_code == 200):
         users = response.json()
