e658940c0386b8be517987beb26aa4742025afcb
Isquare007 <akibrahiim2019@gmail.com>
2023-02-02 10:12:40 +0000

    after 2

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,51 +1,43 @@
 #!/usr/bin/python3
 """Reddit API"""
-from collections import Counter
-import re
-from requests import get
+
+import requests
 
 
-def count_words(subreddit, word_list, after='', found_word=[]):
-    """ recursive function that queries the Reddit API """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {
-        "limit": 100,
-        "after": after
-    }
-    response = get(url, headers={'User-Agent': 'MyBot/0.0.1'}, params=params)
+def count_words(subreddit, word_list, found_list=[], after=None):
+    '''Prints counts of given words found in hot posts of a given subreddit.
+    Args:
+        subreddit (str): The subreddit to search.
+        word_list (list): The list of words to search for in post titles.
+        found_list (obj): Key/value pairs of words/counts.
+        after (str): The parameter for the next page of the API results.
+    '''
+    user_agent = {'User-agent': 'test45'}
+    posts = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                         .format(subreddit, after), headers=user_agent)
     if after is None:
         word_list = [word.lower() for word in word_list]
-    if response.status_code == 200:
-        data = response.json()
-        for post in data['data']['children']:
+
+    if posts.status_code == 200:
+        posts = posts.json()['data']
+        aft = posts['after']
+        posts = posts['children']
+        for post in posts:
             title = post['data']['title'].lower()
             for word in title.split(' '):
                 if word in word_list:
-                    found_word.append(word)
-
-            """for word in word_list:
-                count[word] += len(
-                    re.findall(
-                        "(?<![._]){}(?![._])".format(word),
-                        title))
-        after = data['data']['after']"""
-        after = data['data']['after']
-        if after:
-            count_words(subreddit, word_list, after, found_word)
+                    found_list.append(word)
+        if aft is not None:
+            count_words(subreddit, word_list, found_list, aft)
         else:
-            """if count:
-                for word, count in count.most_common():
-                    if count == 0:
-                        continue
-                    print(word, count)"""
             result = {}
-            for word in found_word:
+            for word in found_list:
                 if word.lower() in result.keys():
                     result[word.lower()] += 1
                 else:
                     result[word.lower()] = 1
-            for key, value in sorted(
-                    result.items(), key=lambda item: item[1], reverse=True):
-                print("{}: {}".format(key, value))
+            for key, value in sorted(result.items(), key=lambda item: item[1],
+                                     reverse=True):
+                print('{}: {}'.format(key, value))
     else:
         return
---
+++ 0x16-api_advanced/100-sub.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Reddit API"""
+from collections import Counter
+import re
+from requests import get
+
+
+def count_words(subreddit, word_list, after='', found_word=[]):
+    """ recursive function that queries the Reddit API """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {
+        "limit": 100,
+        "after": after
+    }
+    response = get(url, headers={'User-Agent': 'MyBot/0.0.1'}, params=params)
+    if after is None:
+        word_list = [word.lower() for word in word_list]
+    if response.status_code == 200:
+        data = response.json()
+        for post in data['data']['children']:
+            title = post['data']['title'].lower()
+            for word in title.split(' '):
+                if word in word_list:
+                    found_word.append(word)
+
+            """for word in word_list:
+                count[word] += len(
+                    re.findall(
+                        "(?<![._]){}(?![._])".format(word),
+                        title))
+        after = data['data']['after']"""
+        after = data['data']['after']
+        if after:
+            count_words(subreddit, word_list, after, found_word)
+        else:
+            """if count:
+                for word, count in count.most_common():
+                    if count == 0:
+                        continue
+                    print(word, count)"""
+            result = {}
+            for word in found_word:
+                if word.lower() in result.keys():
+                    result[word.lower()] += 1
+                else:
+                    result[word.lower()] = 1
+            for key, value in sorted(
+                    result.items(), key=lambda item: item[1], reverse=True):
+                print("{}: {}".format(key, value))
+    else:
+        return
